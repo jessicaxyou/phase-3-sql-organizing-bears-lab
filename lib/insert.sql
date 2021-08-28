04edf14799cff0a5580f99722013540436f06e0f
jessica you <jessicaxytan@gmail.com>
2021-08-27 21:09:46 -0400

    commit tree 9acce57975960748c4bd4540d0514b7bb0b82f88

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES
+("Mr. Chocolate", 4, "M", "Brown", "Melancholic", 1),
+("Rowdy", 1, "F", "Black", "Choleric", 1),
+("Tabitha", 8, "F", "Black", "Phlegmatic", 2),
+("Sergeant Brown", 6, "M", "Brown", "Choleric", 1),
+("Melissa", 10, "F", "Brown", "Sanguine", 2),
+("Grinch", 6, "M", "Red", "Choleric", 2),
+("Wendy", 12, "F", "Red", "Melancholic", 1),
+(NULL, 2, "M", "Black", "Sanguine", 1)
